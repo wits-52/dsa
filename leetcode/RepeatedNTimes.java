0f2f7e0b5485f34b61129f37a1f891d64671f0ee
Ritik Goyal <ritik.goyal987@gmail.com>
2026-01-02 11:16:31 +0530

    961: LC daily AC: RepeatedNTimes time O(n), memory O(1)

--- leetcode/RepeatedNTimes.java
+++ leetcode/RepeatedNTimes.java
@@ -17,8 +17,29 @@ public class RepeatedNTimes {
         }
         return -1;
     }
+    private boolean doesGivenIndexExistInPreviousThree(int[] nums, int idx) {
+        for (int i = idx - 1; i >= Math.max(0, idx - 3); i--) {
+            if (nums[idx] == nums[i]) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+    private int solveWithLinearTimeAndConstantMemoryComplexity(int[] nums) {
+        int n = nums.length;
+        if (n % 2 == 1 || n < 4) return -1; // According to problem constraints it is not possible though.
+
+        for (int i = 1; i < nums.length; i++) {
+            if (this.doesGivenIndexExistInPreviousThree(nums, i)) {
+                return nums[i];
+            }
+        }
+
+        return -1;
+    }
     public int repeatedNTimes(int[] nums) {
-        return solveWithLinearTimeAndMemoryComplexity(nums);
+        return solveWithLinearTimeAndConstantMemoryComplexity(nums);
     }
     public static void main(String[] args) {
         RepeatedNTimes solution = new RepeatedNTimes();
@@ -26,5 +47,7 @@ public class RepeatedNTimes {
         TestUtil.run("Test Case #1", 3, solution.repeatedNTimes(new int[]{1, 2, 3, 3}));
         TestUtil.run("Test Case #2", 2, solution.repeatedNTimes(new int[]{2, 1, 2, 5, 3, 2}));
         TestUtil.run("Test Case #3", 5, solution.repeatedNTimes(new int[]{5, 1, 5, 2, 5, 3, 5, 4}));
+        TestUtil.run("Test Case #4", 9, solution.repeatedNTimes(new int[]{9, 5, 6, 9}));
+        TestUtil.run("Test Case #5", 2, solution.repeatedNTimes(new int[]{2, 6, 2, 1}));
     }
 }
