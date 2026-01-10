da6eecb839e1790544c8d2e9ee303b07fd922d1f
Ritik Goyal <ritik.goyal987@gmail.com>
2026-01-10 12:53:57 +0530

    final solution MaxDotProductOfTwoSubsequences

--- leetcode/daily/MaxDotProductOfTwoSubsequences.java
+++ leetcode/daily/MaxDotProductOfTwoSubsequences.java
@@ -3,53 +3,15 @@ package leetcode.daily;
 import utils.TestUtil;
 
 public class MaxDotProductOfTwoSubsequences {
-    private boolean allNegative(int[] nums) {
-        for (int n: nums) {
-            if (n >= 0) {
-                return false;
-            }
-        }
-        return true;
-    }
-    private boolean allPositive(int[] nums) {
-        for (int n: nums) {
-            if (n <= 0) {
-                return false;
-            }
-        }
-        return true;
-    }
-    private int min(int[] nums) {
-        int val = nums[0];
-        for (int n: nums) {
-            if (n < val) {
-                val = n;
-            }
-        }
-        return val;
-    }
-    private int max(int[] nums) {
-        int val = nums[0];
-        for (int n: nums) {
-            if (n > val) {
-                val = n;
-            }
-        }
-        return val;
+    private int max(int a, int b, int c, int d) {
+        return Math.max(Math.max(Math.max(a, b), c), d);
     }
     public int maxDotProduct(int[] nums1, int[] nums2) {
-        if (allNegative(nums1) && allPositive(nums2)) {
-            return max(nums1) * min(nums2);
-        }
-        if (allNegative(nums2) && allPositive(nums1)) {
-            return max(nums2) * min(nums1);
-        }
-
         int cols = nums1.length, rows = nums2.length;
 
         int[][] maxDotProd = new int[rows][cols];
 
-        maxDotProd[rows - 1][cols - 1] = Math.max(nums2[rows - 1] * nums1[cols - 1], 0);
+        maxDotProd[rows - 1][cols - 1] = nums2[rows - 1] * nums1[cols - 1];
 
         for (int row = rows - 2; row >= 0; row--) {
             maxDotProd[row][cols - 1] = Math.max(
@@ -65,11 +27,10 @@ public class MaxDotProductOfTwoSubsequences {
         }
         for (int i = rows - 2; i >= 0; i--) {
             for (int j = cols - 2; j >=0; j--) {
-                maxDotProd[i][j] = Math.max(
-                    Math.max(
-                        maxDotProd[i + 1][j],
-                        (nums2[i] * nums1[j]) + maxDotProd[i+1][j+1]
-                    ),
+                maxDotProd[i][j] = this.max(
+                    nums2[i] * nums1[j],
+                    maxDotProd[i + 1][j],
+                    (nums2[i] * nums1[j]) + maxDotProd[i+1][j+1],
                     maxDotProd[i][j+1]
                 );
             }
