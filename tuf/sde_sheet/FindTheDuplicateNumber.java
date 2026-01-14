fdb2d4c8d0eb96771ae29e00cb031c47d7765b81
Ritik Goyal <ritik.goyal987@gmail.com>
2026-01-14 20:11:58 +0530

    TUF SDE sheet #10: FindTheDuplicateNumber;AC; Marking array Time: O(N); Memory:O(1)

---
+++ tuf/sde_sheet/FindTheDuplicateNumber.java
@@ -0,0 +1,29 @@
+package tuf.sde_sheet;
+
+import utils.TestUtil;
+
+public class FindTheDuplicateNumber {
+    private int markingArray(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            int num = Math.abs(nums[i]);
+
+            if (nums[num] < 0) {
+                return num;
+            }
+
+            nums[num] *= -1;
+        }
+        return -1;
+    }
+    public int findDuplicate(int[] nums) {
+        return this.markingArray(nums);
+    }
+
+    public static void main(String[] args) {
+        FindTheDuplicateNumber solution = new FindTheDuplicateNumber();
+
+        TestUtil.run("Test Case #1", 2, solution.findDuplicate(new int[]{1,3,4,2,2}));
+        TestUtil.run("Test Case #2", 3, solution.findDuplicate(new int[]{3,1,3,4,2}));
+        TestUtil.run("Test Case #3", 3, solution.findDuplicate(new int[]{3,3,3,3,3}));
+    }
+}
