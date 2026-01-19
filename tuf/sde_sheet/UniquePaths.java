14ee967c5761823aef38e545f1216179a726355b
Ritik Goyal <ritik.goyal987@gmail.com>
2026-01-19 22:37:04 +0530

    TUF SDE sheet #17: UniquePaths; AC; Time: O(NM); Memory: O(N)

--- tuf/sde_sheet/UniquePaths.java
+++ tuf/sde_sheet/UniquePaths.java
@@ -23,11 +23,29 @@ public class UniquePaths {
 
         return noOfPathFrom[0][0];
     }
+    public int uniquePaths(int m, int n, boolean optimiseSpace) {
+        if (!optimiseSpace) {
+            return this.uniquePaths(m, n);
+        }
+        int[][] noOfPathFrom = new int[1][n];
+
+        for (int j = 0; j < n; j++) {
+            noOfPathFrom[0][j] = 1;
+        }
+
+        for (int i = m-2; i >= 0; i--) {
+            for (int j = n-2; j >= 0; j--) {
+                noOfPathFrom[0][j] = noOfPathFrom[0][j] + noOfPathFrom[0][j+1];
+            }
+        }
+
+        return noOfPathFrom[0][0];
+    }
 
     public static void main(String[] args) {
         UniquePaths solution = new UniquePaths();
 
-        TestUtil.run("Test Case #1", 28, solution.uniquePaths(3, 7));
-        TestUtil.run("Test Case #2", 3, solution.uniquePaths(3, 2));
+        TestUtil.run("Test Case #1", 28, solution.uniquePaths(3, 7, true));
+        TestUtil.run("Test Case #2", 3, solution.uniquePaths(3, 2, true));
     }
 }
