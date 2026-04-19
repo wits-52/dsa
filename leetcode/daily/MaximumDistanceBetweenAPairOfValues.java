75835d1be9318570bfff4f81fcc0c63783d16a1e
Ritik Goyal <ritik.goyal987@gmail.com>
2026-04-19 22:30:01 +0530

    LD daily: Maximum Distance Between a Pair of Values; O(n+m)

--- leetcode/daily/MaximumDistanceBetweenAPairOfValues.java
+++ leetcode/daily/MaximumDistanceBetweenAPairOfValues.java
@@ -4,25 +4,14 @@ import utils.TestUtil;
 
 public class MaximumDistanceBetweenAPairOfValues {
     public int maxDistance(int[] nums1, int[] nums2) {
-        int num2Start = 0, maxDistance = 0;
+        int i = 0, j = 0, maxDistance = 0;
 
-        for (int i = 0; i < nums1.length && i < nums2.length; i++) {
-            int start = Math.max(i, num2Start), end = nums2.length - 1;
-
-            while (start < end) {
-                int mid = (int)Math.ceil((start + end) / 2.0);
-
-                if (nums2[mid] < nums1[i]) {
-                    end = mid - 1;
-                } else {
-                    start = mid;
-                }
-            }
-
-            if (nums2[start] >= nums1[i]) {
-                num2Start = start;
-
-                maxDistance = Math.max(maxDistance, start - i);
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] <= nums2[j]) {
+                maxDistance = Math.max(j - i, maxDistance);
+                j++;
+            } else {
+                i++;
             }
         }
 
